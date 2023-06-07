e31fc9d6c81aca3a95349e6ad2fd5c3818989f91
Gevorg Martirosyan <Gevorg_Martirosyan@epamyerw02fe.epam.com>
2023-06-07 13:31:14 +0400

    move config values into env and add examples in .env-example

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,15 +1,17 @@
 import { DataSource } from 'typeorm';
+import 'dotenv/config';
+
 
 class Database {
     private dataSource;
 
     constructor() {
         this.dataSource = new DataSource({
-            type: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "username",
-            password: "password",
+            type: process.env.PG_TYPE as string as any,
+            host: process.env.HOST,
+            port: Number(process.env.DB_PORT),
+            username: process.env.USERNAME,
+            password: process.env.PASSWORD,
             database: "database",
             synchronize: true,
             entities: ["src/entity/**/*.ts"]
